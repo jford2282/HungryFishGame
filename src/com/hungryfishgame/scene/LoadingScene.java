f40ea30051bb9bf9788c9a6f7190b9e3c0e58756
jford2282 <jford2282@yahoo.com>
2014-09-10 21:25:00 -0400

    BUG FIX

---
+++ src/com/hungryfishgame/scene/LoadingScene.java
@@ -0,0 +1,47 @@
+package com.hungryfishgame.scene;
+
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.text.Text;
+import org.andengine.util.adt.color.Color;
+ 
+import com.hungryfishgame.GameActivity;
+import com.hungryfishgame.manager.SceneManager.SceneType;
+
+public class LoadingScene extends BaseScene{
+	
+	@Override
+    public void createScene()
+    {
+            setBackground(new Background(Color.WHITE));
+            attachChild(new Text(GameActivity.CAMERA_WIDTH/2, GameActivity.CAMERA_HEIGHT/2, resourceManager.font, "Loading...", vbom));
+    }
+
+    @Override
+    public void onBackKeyPressed()
+    {
+            return;
+    }
+
+    @Override
+    public SceneType getSceneType()
+    {
+            return SceneType.SCENE_LOADING;
+    }
+
+    @Override
+    public void disposeScene()
+    {
+
+    }
+
+    @Override
+    public void resume() {
+           
+    }
+
+    @Override
+    public void pause() {
+           
+    }
+
+}
